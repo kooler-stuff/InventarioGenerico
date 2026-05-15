a3b2a2957e2d006c031cd76881cba559087c1220
kooler-stuff <maximodcuevas@gmail.com>
2026-05-15 15:59:25 -0300

    Add files via upload

--- models/insumo.js
+++ models/insumo.js
@@ -4,7 +4,8 @@ const InsumoSchema = new mongoose.Schema({
     categoria: { type: String, required: true },
     insumo: { type: String, required: true },
     unidades: { type: Number, default: 0 },
-    fecha: { type: Date, default: Date.now }
+    fecha: { type: Date, default: Date.now },
+    imagen: { type: String, required: false }
 });
 
 module.exports = mongoose.model('Insumo', InsumoSchema);
