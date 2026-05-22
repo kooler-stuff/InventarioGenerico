9f81e149b665f3beaa45b6a074736721d24aef00
kooler-stuff <maximodcuevas@gmail.com>
2026-05-22 16:00:22 -0300

    Add files via upload

--- models/users.js
+++ models/users.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const UserCredentialsSchema = new mongoose.Schema({
     usuario: { type: String, required: true },
     contraseña: { type: String, required: true },
+    acceso: {type: String, required: true}
 });
 
 module.exports = mongoose.model('user_credentials', UserCredentialsSchema);
